7d713f81cb17ada82aef74bbfcebd18508587938
Mariam Sayed <mariamsayed146@gmail.com>
2026-08-31 06:46:54 +0300

    Add api-gateway: routing, service discovery, load balancing

---
+++ api-gateway/src/main/java/com/ecommerce/gateway/api_gateway/GatewayLoggingFilter.java
@@ -0,0 +1,29 @@
+package com.ecommerce.gateway.api_gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GatewayLoggingFilter implements GlobalFilter, Ordered {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("request -> {} {}",
+                exchange.getRequest().getMethod(),
+                exchange.getRequest().getURI());
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 1;
+    }
+}
